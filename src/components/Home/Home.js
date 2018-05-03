01c32bbfd711b0f2bc8a174a161bc3f65cff8d44
eneax <19818937+eneax@users.noreply.github.com>
2018-05-03 12:02:44 +0200

    Update subheader

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,8 @@ const home = () => {
 	return (
 		<main>
 			<div className="tc pv4 pv5-ns">
-			  <h1 className="f4 f3-ns fw6 dark-gray courier">Hi, I"m Enea!</h1>
-			  <h2 className="f6 mid-gray fw2 ttu tracked georgia">Self-taught Web Developer</h2>
+			  <h1 className="f4 f3-ns fw6 dark-gray courier">Hi, I'm Enea!</h1>
+			  <h2 className="f6 mid-gray fw2 ttu tracked georgia">Web Developer</h2>
 			  
 			  <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" href="https://github.com/eneax" target="_blank" rel="noopener noreferrer" >
 			  	<i className="fab fa-github fa-2x"></i>
