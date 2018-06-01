7c00bba620e6a2f6ca2967ed54198bba2e522c05
eneax <19818937+eneax@users.noreply.github.com>
2018-06-01 16:39:24 +0200

    Add github-battle to projectsList

--- src/components/Projects/projectsList.js
+++ src/components/Projects/projectsList.js
@@ -52,6 +52,56 @@ export default [
     )
   },
   {
+    name: 'github-battle',
+    render: (
+      <div className="fl w-100 w-50-m w-third-ns pa2">
+        <div className="bg-washed-blue br3 grow shadow-5">
+          <div className="center mw6 br3 hidden mv3 tc">
+            <h1 className="f5 bg-washed-green br3 br--top black-70 mv0 pv2 ph3 courier">
+              github-battle
+            </h1>
+            <div className="pa3 bt b--black-10 black-70 georgia">
+              <p className="f6 f5-ns lh-copy measure">
+                Discover popular repos and battle your Github friends
+              </p>
+            </div>
+            <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" target="_blank" rel="noopener noreferrer" href="https://github.com/eneax/github-battle">
+              <i className="fab fa-github fa-2x"></i>
+            </a>
+            <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" target="_blank" rel="noopener noreferrer" href="https://github-battle-x.firebaseapp.com">
+              <i className="fas fa-globe fa-2x"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  },
+  {
+    name: 'resume-theme',
+    render: (
+      <div className="fl w-100 w-50-m w-third-ns pa2">
+        <div className="bg-washed-blue br3 grow shadow-5">
+          <div className="center mw6 br3 hidden mv3 tc">
+            <h1 className="f5 bg-washed-green br3 br--top black-70 mv0 pv2 ph3 courier">
+              resume-theme
+            </h1>
+            <div className="pa3 bt b--black-10 black-70 georgia">
+              <p className="f6 f5-ns lh-copy measure">
+                A fully responsive resume theme built with Bootstrap
+              </p>
+            </div>
+            <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" target="_blank" rel="noopener noreferrer" href="https://github.com/eneax/resume-theme">
+              <i className="fab fa-github fa-2x"></i>
+            </a>
+            <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" target="_blank" rel="noopener noreferrer" href="https://eneax.github.io/resume-theme">
+              <i className="fas fa-globe fa-2x"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  },
+  {
     name: 'recognizer',
     render: (
       <div className="fl w-100 w-50-m w-third-ns pa2">
@@ -100,58 +150,5 @@ export default [
         </div>
       </div>
     )
-  },
-  {
-    name: 'portfolio-theme',
-    render: (
-      <div className="fl w-100 w-50-m w-third-ns pa2">
-        <div className="bg-washed-blue br3 grow shadow-5">
-          <div className="center mw6 br3 hidden mv3 tc">
-            <h1 className="f5 bg-washed-green br3 br--top black-70 mv0 pv2 ph3 courier">
-              portfolio-theme
-            </h1>
-            <div className="pa3 bt b--black-10 black-70 georgia">
-              <p className="f6 f5-ns lh-copy measure">
-                A fully responsive portfolio theme built with Bootstrap
-              </p>
-            </div>
-            <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" target="_blank" rel="noopener noreferrer" href="https://github.com/eneax/portfolio-theme">
-              <i className="fab fa-github fa-2x"></i>
-            </a>
-            <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" target="_blank" rel="noopener noreferrer" href="https://eneax.github.io/portfolio-theme">
-              <i className="fas fa-globe fa-2x"></i>
-            </a>
-          </div>
-        </div>
-      </div>
-    )
-  },
-  {
-    name: 'resume-theme',
-    render: (
-      <div className="fl w-100 w-50-m w-third-ns pa2">
-        <div className="bg-washed-blue br3 grow shadow-5">
-          <div className="center mw6 br3 hidden mv3 tc">
-            <h1 className="f5 bg-washed-green br3 br--top black-70 mv0 pv2 ph3 courier">
-              resume-theme
-            </h1>
-            <div className="pa3 bt b--black-10 black-70 georgia">
-              <p className="f6 f5-ns lh-copy measure">
-                A fully responsive resume theme built with Bootstrap
-              </p>
-            </div>
-            <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" target="_blank" rel="noopener noreferrer" href="https://github.com/eneax/resume-theme">
-              <i className="fab fa-github fa-2x"></i>
-            </a>
-            <a className="f6 grow dib v-mid ph3 pv2 mb3 dark-gray" target="_blank" rel="noopener noreferrer" href="https://eneax.github.io/resume-theme">
-              <i className="fas fa-globe fa-2x"></i>
-            </a>
-          </div>
-        </div>
-      </div>
-    )
-  },
+  }
 ];
-
-
- 
