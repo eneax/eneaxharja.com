2456a61ebbe508a5219dd5c802c2233402877058
eneax <19818937+eneax@users.noreply.github.com>
2018-07-03 21:13:37 +0200

    Update resume link and add krix to project list

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,18 +7,18 @@ export default [
     link: '/projects#'
   },
   {
+    title: 'krix-portfolio',
+    descOne: 'Clean and minimal portfolio ',
+    descTwo: ' website built with React',
+    repo: 'https://github.com/eneax/krix-portfolio',
+    link: 'https://gentle-bastion-82801.herokuapp.com'
+  },
+  {
     title: 'thriving-cities',
     descOne: 'Single page company website built ',
     descTwo: ' with React and Bootstrap',
     repo: 'https://github.com/eneax/thriving-cities',
     link: 'https://tranquil-shore-37678.herokuapp.com'
-  },
-  {
-    title: 'github-battle',
-    descOne: 'Discover popular repos and battle ',
-    descTwo: ' your Github friends',
-    repo: 'https://github.com/eneax/github-battle',
-    link: 'https://github-battle-x.firebaseapp.com'
   },
   {
     title: 'weather-app',
@@ -28,17 +28,17 @@ export default [
     link: 'https://weather-app-x.firebaseapp.com'
   },
   {
+    title: 'github-battle',
+    descOne: 'Discover popular repos and battle ',
+    descTwo: ' your Github friends',
+    repo: 'https://github.com/eneax/github-battle',
+    link: 'https://github-battle-x.firebaseapp.com'
+  },
+  {
     title: 'recognizer',
     descOne: 'Face Recognition App ',
     descTwo: ' (front-end code)',
     repo: 'https://github.com/eneax/recognizer',
-    link: 'https://recognizer-x.herokuapp.com'
-  },
-  {
-    title: 'recognizer-api',
-    descOne: 'Face Recognition App ',
-    descTwo: ' (back-end code)',
-    repo: 'https://github.com/eneax/recognizer-api',
     link: 'https://recognizer-x.herokuapp.com'
   }
 ];
