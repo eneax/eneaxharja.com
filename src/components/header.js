a56ac5eacd8a54efe01ee21a1a0ce75f4cbc048f
eneax <19818937+eneax@users.noreply.github.com>
2018-07-26 14:53:01 +0200

    Create About page

--- src/components/header.js
+++ src/components/header.js
@@ -2,32 +2,42 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+	<div
+		style={{
+			background: 'rebeccapurple',
+			marginBottom: '1.45rem',
+		}}
+	>
+		<div
+			style={{
+				margin: '0 auto',
+				maxWidth: 960,
+				padding: '1.45rem 1.0875rem',
+			}}
+		>
+			<h1 style={{ margin: 0 }}>
+				<Link
+					to="/"
+					style={{
+						color: 'white',
+						textDecoration: 'none',
+					}}
+				>
+					{siteTitle}
+				</Link>
+			</h1>
+		</div>
+		<nav>
+			<ul>
+				<li>
+					<Link to='/'>Home</Link>
+				</li>
+				<li>
+					<Link to='/about'>About</Link>
+				</li>
+			</ul>
+		</nav>
+	</div>
 )
 
 export default Header
