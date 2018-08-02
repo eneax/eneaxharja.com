4462e2726e3299b25ac07d8b0f4049827e6e05d1
eneax <19818937+eneax@users.noreply.github.com>
2018-08-02 15:54:29 +0200

    Refactor projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,59 @@
 import React, { Component } from 'react'
 import { Container, Row, Col, TitleWrapper, SubTitleWrapper, MainTextWrapper, ImageWrapper } from '../components/General'
 import Footer from '../components/Footer'
-import projects from '../libs/projects-data'
+
+import projectOne from '../images/projects/project1.png'
+import projectTwo from '../images/projects/project2.png'
+import projectThree from '../images/projects/project3.png'
+import projectFour from '../images/projects/project4.png'
+import projectFive from '../images/projects/project5.png'
+import projectSix from '../images/projects/project6.png'
+
+
+const projects = [
+  {
+    repo: 'https://github.com/eneax/eneax.github.io',
+    title: 'eneaxharja[dot]com',
+    link: 'https://github.com/eneax/eneax.github.io',
+    img: projectOne,
+    desc: 'My Own Little Corner of the Web. Designed and coded on a MacBook Pro in Tuscany (Italy). Built on Gatsby, the blazing-fast static site generator for React, and coded with VS Code.',
+  },
+  {
+    repo: 'https://github.com/eneax/minimal-blog',
+    title: 'Minimal Blog',
+    link: 'https://minimal-blog-x.netlify.com',
+    img: projectTwo,
+    desc: 'A minimal blog built with Gatsby, Netlify CMS, GraphQL and Web Animations API.',
+  },
+  {
+    repo: 'https://github.com/eneax/globetrotter',
+    title: 'Globetrotter',
+    link: 'https://aqueous-badlands-68133.herokuapp.com',
+    img: projectThree,
+    desc: 'Responsive travel agency website made using React and React Router v4.',
+  },
+  {
+    repo: 'https://github.com/eneax/krix-portfolio',
+    title: 'Portfolio Website',
+    link: 'https://gentle-bastion-82801.herokuapp.com',
+    img: projectFour,
+    desc: 'Clean and minimal portfolio website built with React and React Router v4.',
+  },
+  {
+    repo: 'https://github.com/eneax/thriving-cities',
+    title: 'Thriving Cities',
+    link: 'https://tranquil-shore-37678.herokuapp.com',
+    img: projectFive,
+    desc: 'This project represents a responsive single page company website built with React and Bootstrap.',
+  },
+  {
+    repo: 'https://github.com/eneax/weather-app',
+    title: 'Weather App',
+    link: 'https://weather-app-x.firebaseapp.com',
+    img: projectSix,
+    desc: 'A functioning Weather app built with React and Open Weather API.',
+  }
+]
 
 
 const ProjectItem = ({ repo, title, link, img, desc }) => {
