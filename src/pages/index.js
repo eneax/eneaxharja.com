64c7b825228e954b3f2546727ad984448263a722
eneax <19818937+eneax@users.noreply.github.com>
2018-08-10 22:09:34 +0200

    Add aria-label to homepage links

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ export default class Index extends Component {
       <HeaderWrapper>
         <Header>Hi, I'm Enea!</Header>
         <SubHeader>Web Developer</SubHeader>
-        <SocialLink href="https://github.com/eneax" target="_blank" rel="noopener noreferrer" >
+        <SocialLink href="https://github.com/eneax" aria-label="github" target="_blank" rel="noopener noreferrer" >
           <i className="fab fa-github fa-2x"></i>
         </SocialLink>
-        <SocialLink href="mailto:eneaxharja@gmail.com" target="_blank" rel="noopener noreferrer" >
+        <SocialLink href="mailto:eneaxharja@gmail.com" aria-label="mail" target="_blank" rel="noopener noreferrer" >
           <i className="fas fa-envelope fa-2x"></i>
         </SocialLink>
       </HeaderWrapper>
