5b932c22c370d39c7a0b037ea1d8daea9e40ae42
eneax <19818937+eneax@users.noreply.github.com>
2018-08-10 22:00:59 +0200

    Add box shadow to library and projects images

--- src/components/General.js
+++ src/components/General.js
@@ -183,6 +183,9 @@ const Thumbnail = styled.div`
 	max-width: 100%;
 	height: auto;
 	text-align: center;
+	box-shadow: 0 16px 24px 2px rgba(0, 0, 0, .14), 
+							0 6px 30px 5px rgba(0, 0, 0, .12), 
+							0 8px 10px -5px rgba(0, 0, 0, .2);
 	a {
 		color: #000;
 		text-decoration: none;
@@ -305,6 +308,9 @@ const ImageWrapper = styled.div`
 		margin-bottom: .75rem;
 		border: 1px solid #e7e7e7;
 		border-radius: 0.25rem !important;
+		box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .14), 
+								0 3px 1px -2px rgba(0, 0, 0, .2), 
+								0 1px 5px 0 rgba(0, 0, 0, .12);
 	}
 `
 
