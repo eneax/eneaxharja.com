9499441c748b20a3394b2a04015b67f848854a01
eneax <19818937+eneax@users.noreply.github.com>
2018-08-24 16:59:37 +0200

    Test use unsplash img for projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 import { Container, Row, Col, TitleWrapper, SubTitleWrapper, MainTextWrapper, ImageWrapper } from '../components/General'
 import Footer from '../components/Footer'
 
-import projectOne from '../images/projects/project1.png'
-import projectTwo from '../images/projects/project2.png'
-import projectThree from '../images/projects/project3.png'
-import projectFour from '../images/projects/project4.png'
-import projectFive from '../images/projects/project5.png'
-import projectSix from '../images/projects/project6.png'
+// import projectOne from '../images/projects/project1.png'
+// import projectTwo from '../images/projects/project2.png'
+// import projectThree from '../images/projects/project3.png'
+// import projectFour from '../images/projects/project4.png'
+// import projectFive from '../images/projects/project5.png'
+// import projectSix from '../images/projects/project6.png'
 
 
 const projects = [
@@ -15,42 +15,42 @@ const projects = [
     repo: 'https://github.com/eneax/eneax.github.io',
     title: 'eneaxharja[dot]com',
     link: 'https://github.com/eneax/eneax.github.io',
-    img: '',
+    img: 'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a20bc6324f6ef2969d9a7cae56b8d4d1&auto=format&fit=crop&w=1650&q=80',
     desc: 'My Own Little Corner of the Web. Designed on a MacBook Pro in Tuscany (Italy). Built on Gatsby, the blazing-fast static site generator for React, and coded with VS Code.',
   },
   {
     repo: 'https://github.com/eneax/minimal-blog',
     title: 'Minimal Blog',
     link: 'https://minimal-blog-x.netlify.com',
-    img: '',
+    img: 'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a20bc6324f6ef2969d9a7cae56b8d4d1&auto=format&fit=crop&w=1650&q=80',
     desc: 'A minimal blog built with Gatsby, Netlify CMS, GraphQL and Web Animations API.',
   },
   {
     repo: 'https://github.com/eneax/globetrotter',
     title: 'Globetrotter',
     link: 'https://aqueous-badlands-68133.herokuapp.com',
-    img: '',
+    img: 'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a20bc6324f6ef2969d9a7cae56b8d4d1&auto=format&fit=crop&w=1650&q=80',
     desc: 'Responsive travel agency website made using React and React Router v4.',
   },
   {
     repo: 'https://github.com/eneax/krix-portfolio',
     title: 'Portfolio Website',
     link: 'https://gentle-bastion-82801.herokuapp.com',
-    img: '',
+    img: 'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a20bc6324f6ef2969d9a7cae56b8d4d1&auto=format&fit=crop&w=1650&q=80',
     desc: 'Clean and minimal portfolio website built with React and React Router v4.',
   },
   {
     repo: 'https://github.com/eneax/thriving-cities',
     title: 'Thriving Cities',
     link: 'https://tranquil-shore-37678.herokuapp.com',
-    img: '',
+    img: 'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a20bc6324f6ef2969d9a7cae56b8d4d1&auto=format&fit=crop&w=1650&q=80',
     desc: 'This project represents a responsive single page company website built with React and Bootstrap.',
   },
   {
     repo: 'https://github.com/eneax/weather-app',
     title: 'Weather App',
     link: 'https://weather-app-x.firebaseapp.com',
-    img: '',
+    img: 'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a20bc6324f6ef2969d9a7cae56b8d4d1&auto=format&fit=crop&w=1650&q=80',
     desc: 'A functioning Weather app built with React and Open Weather API.',
   }
 ]
