0f29f3ce1afeb95895f33bd54456185a10db9d35
eneax <19818937+eneax@users.noreply.github.com>
2018-12-05 21:05:36 +0100

    Fix Netlify building error

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,6 +15,69 @@ import { faEnvelope, faCodeBranch } from '@fortawesome/free-solid-svg-icons'
 library.add(faEnvelope, faCodeBranch)
 
 
+
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Self-taught web developer with proven ability to build for the modern web while using cutting-edge technologies' },
+            { name: 'keywords', content: 'web developer, webdev, gatsbyjs, reactjs, frontend, library, books' },
+          ]}
+          link={[
+            { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+
+        <Particles
+          style={{
+            position: 'fixed',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: -1
+          }}
+          params={particlesConfig}
+        />
+
+        <Header />
+        
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
+        >
+          {children}
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
+
 const particlesConfig = {
   "particles": {
     "number": {
@@ -125,64 +188,3 @@ const particlesConfig = {
   },
   "retina_detect": true
 }
-
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Self-taught web developer with proven ability to build for the modern web while using cutting-edge technologies' },
-            { name: 'keywords', content: 'web developer, webdev, gatsbyjs, reactjs, frontend, library, books' },
-          ]}
-          link={[
-            { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-
-        <Particles
-          style={{
-            position: 'fixed',
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-            zIndex: -1
-          }}
-          params={particlesConfig}
-        />
-
-        <Header />
-        
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
-      </>
-    )}
-  />
-)
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
