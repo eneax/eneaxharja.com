ec9025f7988e371ca77c5bea495bcfd91aaf70d2
eneax <19818937+eneax@users.noreply.github.com>
2018-12-06 10:47:54 +0100

    Refactor SearchBox component

--- src/components/General.js
+++ src/components/General.js
@@ -309,41 +309,6 @@ const ImageWrapper = styled.div`
 	}
 `
 
-const SearchInput = styled.input`
-	margin: 1rem auto;
-  padding: 0.375rem 0.75rem;
-	font-family: avenir,sans-serif;
-  font-size: 1rem;
-  line-height: 1.5;
-  background-color: #fff;
-  background-clip: padding-box;
-  border: 1px solid #ced4da;
-  border-radius: 0.25rem;
-  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-	
-	&:focus {
-		color: #495057;
-		background-color: #fff;
-		border-color: #663399;
-		outline: 0;
-		box-shadow: 0 0 0 0.2rem rgba(102, 51, 153, 0.25);
-	}
-	&::-webkit-input-placeholder,
-	&::-moz-placeholder,
-	&:-ms-input-placeholder,
-	&::-ms-input-placeholder,
-	&::placeholder {
-		color: #6c757d;
-		opacity: 1;
-	}
-	&:disabled, 
-	&[readonly] {
-		background-color: #e9ecef;
-		opacity: 1;
-	}
-}
-`
-
 
 
 export {
@@ -361,6 +326,5 @@ export {
 	TitleWrapper,
 	SubTitleWrapper,
 	MainTextWrapper,
-	ImageWrapper,
-	SearchInput
+	ImageWrapper
 }
