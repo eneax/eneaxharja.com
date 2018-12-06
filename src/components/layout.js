5c90a79e0ee64659f79aaa631c314c304022a200
eneax <19818937+eneax@users.noreply.github.com>
2018-12-06 15:35:37 +0100

    Fix Netlify building error

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import './Layout.css'
+import './layout.css'
 
 import Particles from 'react-particles-js';
 import favicon from '../images/favicon.ico'
