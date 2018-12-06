3e0a247978cdb2262b9d366ef163f21ca4afc22f
eneax <19818937+eneax@users.noreply.github.com>
2018-12-06 10:54:04 +0100

    Refactor homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,64 @@
 import React from 'react'
 import Layout from '../components/layout'
-
-import { HeaderWrapper, Header, SubHeader, SocialLink } from '../components/general'
+import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const HeaderWrapper = styled.header`
+	text-align: center;
+	padding-top: 2rem;
+	padding-bottom: 2rem;
+	@media screen and (min-width: 30em) {
+    padding-top: 4rem;
+    padding-bottom: 4rem;
+	}
+	margin: 4rem auto;
+`
+
+const Header = styled.h1`
+	font-size: 1.25rem;
+	@media screen and (min-width: 30em) {
+    font-size: 1.5rem;
+	}
+	font-weight: 600;
+	font-family: avenir,sans-serif;
+`
+
+const SubHeader = styled.h2`
+	font-size: .875rem;
+	font-weight: 200;
+	text-transform: uppercase;
+	letter-spacing: .1em;
+	font-family: avenir,sans-serif;
+	margin-top: -.5rem;
+  margin-bottom: 1rem;
+`
+
+const SocialLink = styled.a`
+	font-size: .875rem;
+	-moz-osx-font-smoothing: grayscale;
+	backface-visibility: hidden;
+	transform: translateZ(0);
+	transition: transform .25s ease-out;
+	&:hover,
+	&:focus {
+		transform: scale(1.05);
+	}
+	&:active {
+		transform: scale(.9);
+	}
+	display: inline-block;
+	vertical-align: middle;
+	padding-left: .5rem;
+	padding-right: .5rem;
+	padding-top: .5rem;
+	padding-bottom: .5rem;
+	margin-bottom: 1rem;
+	color: #333;
+	svg {
+		font-size: 2em;
+	}
+`
+
 
 const IndexPage = () => (
   <Layout>
