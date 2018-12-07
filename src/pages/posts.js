e095c032dceafd845728971f891228c52b235789
eneax <19818937+eneax@users.noreply.github.com>
2018-12-07 15:26:22 +0100

    Add footer to posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
 import Layout from '../components/layout'
 import PostList from '../components/postList'
+import Footer from '../components/footer'
 
 const Posts = () => (
   <Layout>
     <PostList />
+    <Footer />
   </Layout>
 )
 
