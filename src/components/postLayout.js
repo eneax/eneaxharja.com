2297f5f44afe4aecb2138a86144c943622552cbf
eneax <19818937+eneax@users.noreply.github.com>
2018-12-07 14:15:16 +0100

    Setup postLayout

---
+++ src/components/postLayout.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+
+
+export default class postLayout extends Component {
+  render() {
+    const { markdownRemark } = this.props.data
+
+    return (
+      <Layout>
+        <h1>{markdownRemark.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{
+          __html: markdownRemark.html
+        }} />
+      </Layout>
+    )
+  }
+}
+
+export const query = graphql`
+  query PostQuery($slug: String!) {
+    markdownRemark(frontmatter: {
+      slug: {
+        eq: $slug
+      }
+    }) {
+      html
+      frontmatter {
+        title
+        date
+        slug
+      }
+    }
+  }
+`
