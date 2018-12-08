9f03e566e271880bb92836271737e99199bac93f
eneax <19818937+eneax@users.noreply.github.com>
2018-12-08 20:10:12 +0100

    Update gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,8 +31,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Enea Xharja | Web Developer',
+        short_name: 'Enea Xharja',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
