933eb6b139fd4b31361cb2c6f5bb76da8f40f5e2
eneax <19818937+eneax@users.noreply.github.com>
2018-12-08 18:12:48 +0100

    Refactor postsList

--- src/components/postsList.js
+++ src/components/postsList.js
@@ -24,79 +24,8 @@ const POST_LIST_QUERY = graphql`
   }
 `
 
+
 // styles
-// const Container = styled.div`
-//   width: 100%;
-//   padding-right: 15px;
-//   padding-left: 15px;
-//   margin-right: auto;
-//   margin-left: auto;
-// `
-
-// const Row = styled.div`
-//   display: -ms-flexbox;
-//   display: flex;
-//   -ms-flex-wrap: wrap;
-//   flex-wrap: wrap;
-//   margin-right: -15px;
-//   margin-left: -15px;
-// `
-
-// const Col = styled.div`
-//   position: relative;
-//   width: 100%;
-//   min-height: 1px;
-//   padding-right: 15px;
-//   padding-left: 15px;
-//   @media (min-width: 992px) {
-//     -ms-flex: 0 0 66.666667%;
-//     flex: 0 0 66.666667%;
-//     max-width: 66.666667%;
-//   }
-//   @media (min-width: 768px) {
-//     -ms-flex: 0 0 83.333333%;
-//     flex: 0 0 83.333333%;
-//     max-width: 83.333333%;
-//   }
-//   margin-right: auto !important;
-//   margin-left: auto !important;
-// `
-
-// const PostPreview = styled.div`
-//   a {
-//     color: #212529;
-//     text-decoration: none;
-//     &:focus,
-//     &:hover {
-//       text-decoration: none;
-//       color: #2c7873;
-//     }
-//     h2 {
-//       font-size: 30px;
-//       margin-top: 30px;
-//       margin-bottom: 10px;
-//     }
-//     h3 {
-//       font-weight: 300;
-//       margin: 0 0 10px;
-//     }
-//   }
-//   p {
-//     font-size: 18px;
-//     font-style: italic;
-//     margin-top: 0;
-//     color: #868e96;
-//   }
-//   @media only screen and (min-width: 768px) {
-//     a {
-//       h2 {
-//         font-size: 36px;
-//       }
-//     }
-//   }
-// `
-
-
 const Container = styled.div`
   width: 100%;
   padding-right: 15px;
@@ -172,7 +101,7 @@ const MainTextWrapper = styled.div`
 
 
 const PostList = () => (
-  <>
+  <div>
     <TitleWrapper>
       <h1>Blog</h1>
     </TitleWrapper>
@@ -195,77 +124,7 @@ const PostList = () => (
         ))
       )}
     />
-  </>
+  </div>
 )
 
 export default PostList
-
-
-
-
-
-
-// const PostList = () => (
-//   <StaticQuery 
-//     query={POST_LIST_QUERY}
-//     render={({allMarkdownRemark}) => (
-//       allMarkdownRemark.edges.map(({node}) => (
-
-//         <Container>
-//           <Row>
-//             <Col>
-//               <PostPreview key={node.frontmatter.slug}>
-//                 <Link to={`/posts${node.frontmatter.slug}`}>
-//                   <h2>{node.frontmatter.title}</h2>
-//                   <h3>{node.excerpt}</h3>
-//                 </Link>
-//                 <p>{node.frontmatter.date}</p>
-//               </PostPreview>
-//             </Col>
-//           </Row>
-//         </Container>
-
-//       ))
-//     )}
-//   />
-// )
-
-// export default PostList
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-const PostList = () => (
-  <StaticQuery 
-    query={POST_LIST_QUERY}
-    render={({allMarkdownRemark}) => (
-      allMarkdownRemark.edges.map(({node}) => (
-
-        <article key={node.frontmatter.slug}>
-          
-          <Link to={`/posts${node.frontmatter.slug}`}>
-            <h2>{node.frontmatter.title}</h2>
-          </Link>
-
-          <p>{node.excerpt}</p>
-          
-          <p>{node.frontmatter.date}</p>
-
-        </article>
-
-      ))
-    )}
-  />
-)
-
-
-*/ 
