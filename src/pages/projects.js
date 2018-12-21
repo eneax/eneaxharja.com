e8993c0317fb3ee191b817ec8395e82fbfc95824
eneax <19818937+eneax@users.noreply.github.com>
2018-12-21 22:11:16 +0100

    Refactor projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,65 +4,24 @@ import Layout from '../components/layout'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
 import Footer from '../components/footer'
+import projects from '../utils/projects-data'
 
-
-const Container = styled.div`
-  width: 100%;
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
-	@media (min-width: 576px) {
-    max-width: 540px;
-	}
-	@media (min-width: 768px) {
-    max-width: 720px;
-	}
-	@media (min-width: 992px) {
-    max-width: 960px;
-	}
-	@media (min-width: 1200px) {
-    max-width: 1140px;
-	}
-`
-
-const Row = styled.div`
-	display: -ms-flexbox;
-  display: flex;
-  -ms-flex-wrap: wrap;
-  flex-wrap: wrap;
-  margin-right: -15px;
-  margin-left: -15px;
-`
 
 const Col = styled.div`
-	position: relative;
-	width: 100%;
-	min-height: 1px;
-	padding-right: 15px;
-	padding-left: 15px;
 	> p {
-		margin-bottom: 2rem;
 		color: #555;
-		font-family: avenir,sans-serif;
 	}
 `
 
 const TitleWrapper = styled.div`
-  padding: 1rem 1rem;
-  margin-bottom: 2rem;
-	border-radius: 0.3rem;
 	h1 {
 		font-family: avenir,sans-serif;
-		text-align: center;
-		margin-bottom: 0;
 	}
 `
 
 const SubTitleWrapper = styled.div`
 	font-family: avenir,sans-serif;
-	text-align: left;
-	margin-top: 1rem;
+	margin-top: 2rem;
 	a {
 		text-decoration: none;
 		color: #000;
@@ -81,16 +40,12 @@ const SubTitleWrapper = styled.div`
 `
 
 const MainTextWrapper = styled.div`
-	max-width: 30em;
-	margin-right: auto;
-	margin-left: auto;
 	margin-bottom: 4rem;
-	margin-top: 0;
 	font-size: 1.2rem;
 	text-align: justify;
 	background-color: #fff;
 	font-weight: 100;
-	> p {
+	p {
 		font-family: avenir,sans-serif;
 		@media screen and (min-width: 48em) {
 			font-size: 1.5rem;
@@ -125,24 +80,32 @@ const MainTextWrapper = styled.div`
 			}
 		}
 	}
+  p + p {
+    text-indent: 1rem;
+  }
 `
 
 const ImageWrapper = styled.div`
-	width: 100%;
-	height: auto;
 	margin-bottom: .75rem;
 	img {
-		margin-bottom: .75rem;
 		border: 1px solid #e7e7e7;
-		border-radius: 0.25rem !important;
+		border-radius: .25rem !important;
 	}
 `
 
 
 const Projects = ({ data }) => {
+
+  const imageGallery = Object.values(data)
+  const images = []
+
+  imageGallery.map((img, i) => (
+    images.push(<Img key={i} fluid={img.childImageSharp.fluid} alt={`Image of '${projects[i].title}' project`} />)
+  ))
+
   return (
     <Layout>
-      <Container>
+      
         <TitleWrapper>
           <h1>Projects</h1>
         </TitleWrapper>
@@ -153,96 +116,36 @@ const Projects = ({ data }) => {
             last few months.
           </p>
 
-          <Row>
+          <Col>
 
-            <Col>
-              <SubTitleWrapper>
-                <a href='https://github.com/eneax/eneax.github.io' target="_blank" rel="noopener noreferrer">
-                  <h2>eneaxharja[dot]com</h2>
-                </a>
-              </SubTitleWrapper>
-              <ImageWrapper>
-                <a href='https://github.com/eneax/eneax.github.io' target="_blank" rel="noopener noreferrer">
-                  <Img fluid={data.image1.childImageSharp.fluid} alt='eneaxharja[dot]com Pic' />
-                </a>
-              </ImageWrapper>
-              <p>My Own Little Corner of the Web. Designed on a MacBook Pro in Tuscany (Italy). Built on Gatsby, the blazing-fast static site generator for React, and coded with VS Code.</p>
-            </Col>
+            {
+              projects.map((project, i) => (
 
+                <div key={i}>
+                  <SubTitleWrapper>
+                    <a href={`${projects[i].link}`} target="_blank" rel="noopener noreferrer">
+                      <h2>{projects[i].title}</h2>
+                    </a>
+                  </SubTitleWrapper>
 
-            <Col>
-              <SubTitleWrapper>
-                <a href='https://github.com/eneax/minimal-blog' target="_blank" rel="noopener noreferrer">
-                  <h2>Minimal Blog</h2>
-                </a>
-              </SubTitleWrapper>
-              <ImageWrapper>
-                <a href='https://minimal-blog-x.netlify.com' target="_blank" rel="noopener noreferrer">
-                  <Img fluid={data.image2.childImageSharp.fluid} alt='Minimal Blog Pic' />
-                </a>
-              </ImageWrapper>
-              <p>A minimal blog built with Gatsby, Netlify CMS, GraphQL and Web Animations API.</p>
-            </Col>
+                  <ImageWrapper>
+                    <a href={`${projects[i].link}`} target="_blank" rel="noopener noreferrer">
+                      {
+                        images[i]
+                      }
+                    </a>
+                  </ImageWrapper>
 
-            <Col>
-              <SubTitleWrapper>
-                <a href='https://github.com/eneax/globetrotter' target="_blank" rel="noopener noreferrer">
-                  <h2>Globetrotter</h2>
-                </a>
-              </SubTitleWrapper>
-              <ImageWrapper>
-                <a href='https://aqueous-badlands-68133.herokuapp.com' target="_blank" rel="noopener noreferrer">
-                  <Img fluid={data.image3.childImageSharp.fluid} alt='Globetrotter Pic' />
-                </a>
-              </ImageWrapper>
-              <p>Responsive travel agency website made using React and React Router v4.</p>
-            </Col>
+                  <p>{projects[i].desc}</p>
+                </div>
 
-            <Col>
-              <SubTitleWrapper>
-                <a href='https://github.com/eneax/krix-portfolio' target="_blank" rel="noopener noreferrer">
-                  <h2>Portfolio Website</h2>
-                </a>
-              </SubTitleWrapper>
-              <ImageWrapper>
-                <a href='https://gentle-bastion-82801.herokuapp.com' target="_blank" rel="noopener noreferrer">
-                  <Img fluid={data.image4.childImageSharp.fluid} alt='Portfolio Website Pic' />
-                </a>
-              </ImageWrapper>
-              <p>Clean and minimal portfolio website built with React and React Router v4.</p>
-            </Col>
+              ))
+            }
+            
+          </Col>
 
-            <Col>
-              <SubTitleWrapper>
-                <a href='https://github.com/eneax/thriving-cities' target="_blank" rel="noopener noreferrer">
-                  <h2>Thriving Cities</h2>
-                </a>
-              </SubTitleWrapper>
-              <ImageWrapper>
-                <a href='https://tranquil-shore-37678.herokuapp.com' target="_blank" rel="noopener noreferrer">
-                  <Img fluid={data.image5.childImageSharp.fluid} alt='Thriving Cities Pic' />
-                </a>
-              </ImageWrapper>
-              <p>This project represents a responsive single page company website built with React and Bootstrap.</p>
-            </Col>
-
-            <Col>
-              <SubTitleWrapper>
-                <a href='https://github.com/eneax/weather-app' target="_blank" rel="noopener noreferrer">
-                  <h2>Weather App</h2>
-                </a>
-              </SubTitleWrapper>
-              <ImageWrapper>
-                <a href='https://weather-app-x.firebaseapp.com' target="_blank" rel="noopener noreferrer">
-                  <Img fluid={data.image6.childImageSharp.fluid} alt='Weather App Pic' />
-                </a>
-              </ImageWrapper>
-              <p>A functioning Weather app built with React and Open Weather API.</p>
-            </Col>
-
-          </Row>
         </MainTextWrapper>
-      </Container>
+      
 
       <Footer />
     </Layout>
---
+++ src/utils/projects-data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    link: 'https://github.com/eneax/eneax.github.io',
+    title: 'eneaxharja[dot]com',
+    desc: 'My Own Little Corner of the Web. Designed on a MacBook Pro in Tuscany (Italy). Built on Gatsby, the blazing-fast static site generator for React, and coded with VS Code.'
+  },
+  {
+    link: 'https://github.com/eneax/minimal-blog',
+    title: 'Minimal Blog',
+    desc: 'A minimal blog built with Gatsby, Netlify CMS, GraphQL and Web Animations API.'
+  },
+  {
+    link: 'https://github.com/eneax/globetrotter',
+    title: 'Globetrotter',
+    desc: 'Responsive travel agency website made using React and React Router v4.'
+  },
+  {
+    link: 'https://github.com/eneax/krix-portfolio',
+    title: 'Portfolio Website',
+    desc: 'Clean and minimal portfolio website built with React and React Router v4.'
+  },
+  {
+    link: 'https://github.com/eneax/thriving-cities',
+    title: 'Thriving Cities',
+    desc: 'This project represents a responsive single page company website built with React and Bootstrap.'
+  },
+  {
+    link: 'https://github.com/eneax/weather-app',
+    title: 'Weather App',
+    desc: 'A functioning Weather app built with React and Open Weather API.'
+  }
+]
