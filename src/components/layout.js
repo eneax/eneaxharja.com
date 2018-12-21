1efe521c0cb0b578527c505907caa0625c750ecf
eneax <19818937+eneax@users.noreply.github.com>
2018-12-21 22:12:41 +0100

    Refactor about page

--- src/components/layout.js
+++ src/components/layout.js
@@ -57,10 +57,10 @@ const Layout = ({ children }) => (
         
         <div
           style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            maxWidth: '42rem',
+            padding: '2.625rem 1.3125rem'
           }}
         >
           {children}
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,82 +5,57 @@ import styled from 'styled-components'
 import Footer from '../components/footer'
 
 
-const Container = styled.div`
-  width: 100%;
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
-	@media (min-width: 576px) {
-    max-width: 540px;
-	}
-	@media (min-width: 768px) {
-    max-width: 720px;
-	}
-	@media (min-width: 992px) {
-    max-width: 960px;
-	}
-	@media (min-width: 1200px) {
-    max-width: 1140px;
-	}
-`
-
 const TitleWrapper = styled.div`
-  padding: 1rem 1rem;
   margin-bottom: 2rem;
-	border-radius: 0.3rem;
 	h1 {
 		font-family: avenir,sans-serif;
-		text-align: center;
-		margin-bottom: 0;
 	}
 `
 
 const MainTextWrapper = styled.div`
-	max-width: 30em;
-	margin-right: auto;
-	margin-left: auto;
-	margin-bottom: 4rem;
-	margin-top: 0;
-	font-size: 1.2rem;
-	text-align: justify;
-	background-color: #fff;
-	font-weight: 100;
-	> p {
-		font-family: avenir,sans-serif;
-		@media screen and (min-width: 48em) {
-			font-size: 1.5rem;
-		}
-		line-height: 1.2;
-  	margin-top: 0;
-		margin-bottom: 0;
-		a {
-			color: #000;
-			text-decoration: underline;
-			transition: color .15s ease-in;
-			transition: background-color .15s ease-in-out;
-			&:link,
-			&:visited {
-				transition: color .15s ease-in;
-			}
-			&:hover {
-				transition: color .15s ease-in;
-				transition: background-color .15s ease-in-out;
-				background-color: #000000;
-				color: #fff;
-			}
-			&:active {
-				transition: color .15s ease-in;
-			}
-			&:focus {
-				transition: color .15s ease-in;
-				transition: background-color .15s ease-in-out;
-				outline: 1px dotted #000000;
-				background-color: #000000;
-				color: #fff;
-			}
-		}
-	}
+  margin-bottom: 4rem;
+  font-size: 1.2rem;
+  text-align: justify;
+  background-color: #fff;
+  font-weight: 100;
+  p {
+    font-family: avenir, sans-serif;
+    @media screen and (min-width: 48em) {
+      font-size: 1.5rem;
+    }
+    line-height: 1.2;
+    margin-top: 0;
+    margin-bottom: 0;
+    a {
+      color: #000;
+      text-decoration: underline;
+      transition: color 0.15s ease-in;
+      transition: background-color 0.15s ease-in-out;
+      &:link,
+      &:visited {
+        transition: color 0.15s ease-in;
+      }
+      &:hover {
+        transition: color 0.15s ease-in;
+        transition: background-color 0.15s ease-in-out;
+        background-color: #000000;
+        color: #fff;
+      }
+      &:active {
+        transition: color 0.15s ease-in;
+      }
+      &:focus {
+        transition: color 0.15s ease-in;
+        transition: background-color 0.15s ease-in-out;
+        outline: 1px dotted #000000;
+        background-color: #000000;
+        color: #fff;
+      }
+    }
+  }
+  p + p {
+    text-indent: 1rem;
+  }
 `
 
 
@@ -95,19 +70,20 @@ const About = () => (
         <p>
           I’m currently living in Chiusi (Tuscany), Italy and working
           as a Freelance Web Developer.
-            </p>
+        </p>
         <p>
           Most of my time is spent writing <a href='https://github.com/eneax' target='_blank' rel='noopener noreferrer'>
-            code</a> for the web. I create websites for small businesses and portfolio pages that are responsive and can adapt the layout to a variety of devices and screen sizes.
-            </p>
+            code</a> for the web. I create websites for businesses and portfolio pages that are responsive and can adapt the layout to a variety of devices and screen sizes.
+        </p>
         <p>
-          When I’m not coding, I read <Link to='/library'>books</Link>, procrastinate on <a href='https://waitbutwhy.com/' target='_blank' rel='noopener noreferrer'>Wait But Why</a> and <a href='http://www.ynharari.com/' target='_blank' rel='noopener noreferrer'>Y. N. Harari</a> or just walk around in nature.
-            </p>
+          When I’m not coding, I read <Link to='/library'>books</Link>, procrastinate <Link to='/library'>online</Link> (reading blogs or listening to podcasts) or just <Link to='/library'>travel</Link> and walk around in nature.
+				</p>
+				<p>For those of you who have been asking me about the software or hardware I use, here is my <Link to='/library'>list</Link>.</p>
         <p>
           I'm open to new opportunities and always looking for new
           projects where I can contribute. Feel free to take a look at
-                  my complete resume over <a href='https://www.dropbox.com/s/hog3x7xo5l7pmpo/eneaxharja-resume.pdf?dl=0' target='_blank' rel='noopener noreferrer'>here</a>.
-            </p>
+          my complete <a href='https://www.dropbox.com/s/hog3x7xo5l7pmpo/eneaxharja-resume.pdf?dl=0' target='_blank' rel='noopener noreferrer'>resume</a>.
+        </p>
       </MainTextWrapper>
     </Container>
 
