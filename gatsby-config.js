8d8a21f69bbad2f1cf5d470692291755e6e8d055
eneax <19818937+eneax@users.noreply.github.com>
2019-01-06 17:36:33 +0100

    Fix http/2 lighthouse error

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,18 @@ module.exports = {
     'gatsby-plugin-offline',
     'gatsby-plugin-styled-components',
     // The plugin will add HTTP/2 assets to server push the critical Gatsby scripts
-    `gatsby-plugin-netlify`, // make sure to put last in the array
+    // make sure to put last in the array
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
   ],
 }
