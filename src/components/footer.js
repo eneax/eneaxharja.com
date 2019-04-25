52b38f669b3204206f837d9f79b7ee26d6a9df06
eneax <19818937+eneax@users.noreply.github.com>
2019-04-25 17:16:48 +0200

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -22,7 +22,7 @@ const FooterWrapper = styled.footer`
 
 const Footer = () => (
 	<FooterWrapper>
-		<small>© 2019 <b>Enea Xharja</b>, All Rights Reserved</small>
+		<small>© {new Date().getFullYear()} <b>Enea Xharja</b>, All Rights Reserved</small>
 	</FooterWrapper>
 )
 
