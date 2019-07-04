6c6d4c961b2be37bd20a8abaf0b1552a84d70622
eneax <19818937+eneax@users.noreply.github.com>
2019-07-04 18:43:29 +0200

    Add emoji to projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,137 +1,137 @@
 export default [
   {
     link: 'https://github.com/eneax/gatsby-restaurant',
-    title: 'Delicious',
+    title: '🍔 Delicious',
     desc: 'Responsive restaurant website built with Gatsby, Contentful, GraphQL and styled-components.'
   },
   {
     link: 'https://github.com/eneax/globetrotter',
-    title: 'Globetrotter',
+    title: '🌎 Globetrotter',
     desc: 'Responsive travel agency website built using Gatsby, Markdown, GraphQL and styled-components.'
   },
   {
     link: 'https://github.com/eneax/krix-portfolio',
-    title: 'Portfolio Website',
+    title: '🏛 Portfolio Website',
     desc: 'Clean and minimal portfolio website built with Gatsby and styled with styled-components.'
   },  
   {
     link: 'https://github.com/eneax/minimal-blog',
-    title: 'Minimal Blog',
+    title: '🖋 Minimal Blog',
     desc: 'A minimal blog built with Gatsby, Netlify CMS, GraphQL and Web Animations API.'
   },
   {
     link: 'https://github.com/eneax/eneax.github.io',
-    title: 'eneaxharja[dot]com',
+    title: '🏡 eneaxharja[dot]com',
     desc: 'My Own Little Corner of the Web. Designed on a MacBook Pro in Tuscany (Italy). Built on Gatsby, the blazing-fast static site generator for React, and coded with VS Code.'
   },
   {
     link: 'https://github.com/eneax/thriving-cities',
-    title: 'Thriving Cities',
+    title: '🌃 Thriving Cities',
     desc: 'This project represents a responsive single page company website built with React and Bootstrap.'
   },
   {
     link: 'https://github.com/eneax/weather-app',
-    title: 'Weather App',
+    title: '🌞 Weather App',
     desc: 'A functioning Weather app built with React and Open Weather API.'
   },
   {
     link: 'https://github.com/eneax/github-battle',
-    title: 'Github Battle',
+    title: '⚔️ Github Battle',
     desc: 'Discover popular repos and battle your Github friends.'
   },
   {
     link: 'https://github.com/eneax/resume-theme',
-    title: 'Resume Theme',
+    title: '🔖 Resume Theme',
     desc: 'Clean and simple resume theme built with Bootstrap 4.'
   },
   {
     link: 'https://github.com/eneax/roboheads',
-    title: 'RoboHeads',
+    title: '🤖 RoboHeads',
     desc: 'A fast and responsive React app where you can search robots by name or email.'
   },
   {
     link: 'https://github.com/eneax/portfolio-theme',
-    title: 'Portfolio Theme',
+    title: '📜 Portfolio Theme',
     desc: 'Bootstrap portfolio theme with smooth scrolling and off-canvas navigation.'
   },
   {
     link: 'https://github.com/eneax/restaurant-theme',
-    title: 'Restaurant Theme',
+    title: '🍴Restaurant Theme',
     desc: 'A responsive restaurant theme built with Bootstrap 4.'
   },
   {
     link: 'https://github.com/eneax/budgetApp',
-    title: 'Budget App',
+    title: '📓 Budget App',
     desc: 'Responsive Budget App built with Bootstrap 4 and Javascript.'
   },
   {
     link: 'https://github.com/eneax/confettiDJ',
-    title: 'confettiDJ',
+    title: '🎉 confettiDJ',
     desc: 'Animation and Sound Game with Paper.js and howler.js.'
   },
   {
     link: 'https://github.com/eneax/toDoList',
-    title: 'ToDo List',
+    title: '📌 ToDo List',
     desc: 'Simple jQuery ToDo List.'
   },
   {
     link: 'https://github.com/eneax/rgbGame',
-    title: 'rgbGame',
+    title: '🖍 rgbGame',
     desc: 'Guess the RGB - A JavaScript Color Guessing Game.'
   },
   {
     link: 'https://github.com/eneax/Trattoria_Porsenna_v3',
-    title: 'Trattoria Porsenna v3',
+    title: '🍽 Trattoria Porsenna v3',
     desc: 'Responsive website of Trattoria Porsenna - Version 3.'
   },
   {
     link: 'https://github.com/eneax/CheckList',
-    title: 'CheckList',
+    title: '📝 CheckList',
     desc: 'A basic checklist built with CSS and Javascript.'
   },
   {
     link: 'https://github.com/eneax/Pig_Game',
-    title: 'Pig (dice game)',
+    title: '🐷 Pig (dice game)',
     desc: 'Pig is a simple game you can play with a single 6-sided die.'
   },
   {
     link: 'https://github.com/eneax/scoreKeeper',
-    title: 'scoreKeeper',
+    title: '🏆 scoreKeeper',
     desc: 'Clean and Minimal Score Keeping App.'
   },
   {
     link: 'https://github.com/eneax/doosan-baseball',
-    title: 'Doosan Baseball',
+    title: '⚾ Doosan Baseball',
     desc: 'Responsive website built with Bootstrap 4 - Doosan Baseball.'
   },
   {
     link: 'https://github.com/eneax/Bootstrap_Landing_Page_Slider',
-    title: 'Landing Page Slider',
+    title: '📸 Landing Page Slider',
     desc: 'Landing Page Slider built with Bootstrap.'
   },
   {
     link: 'https://github.com/eneax/Bootstrap_Image_Gallery',
-    title: 'Bootstrap Image Gallery',
+    title: '📷 Bootstrap Image Gallery',
     desc: 'Responsive Image Gallery built with Bootstrap.'
   },
   {
     link: 'https://github.com/eneax/3D_Rotating_Cube',
-    title: '3D Rotating Cube',
+    title: '🛸 3D Rotating Cube',
     desc: 'Rotating Cube with CSS 3D Transforms - Star Wars Theme.'
   },
   {
     link: 'https://github.com/eneax/3D_Menu_Gallery',
-    title: '3D Menu Gallery',
+    title: '🥘 3D Menu Gallery',
     desc: '3D Menu Gallery with CSS Transition and Transform.'
   },
   {
     link: 'https://github.com/eneax/Menu_Gallery',
-    title: 'Menu Gallery',
+    title: '🍝 Menu Gallery',
     desc: 'Menu Gallery with CSS Transition and Transform.'
   },
   {
     link: 'https://github.com/eneax/Wild_And_Beautiful',
-    title: 'Wild And Beautiful',
+    title: '🎨 Wild And Beautiful',
     desc: 'Creating an SVG Animated Line Drawing.'
   }
 ]
