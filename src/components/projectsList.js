03bb981ac1294cb0c82534b38320fcbe9c9aaa2f
eneax <19818937+eneax@users.noreply.github.com>
2019-10-15 10:52:38 +0200

    Create projectList component

---
+++ src/components/projectsList.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react'
+
+import ProjectCard from './projectCard'
+
+
+const ProjectList = ({ projects }) => {
+  return (
+    <Fragment>
+      {
+        projects.map(({ node }, index) => (
+          <ProjectCard 
+            key={index}
+            project={node}
+          />
+        ))
+      }      
+    </Fragment>
+  )
+}
+
+export default ProjectList
