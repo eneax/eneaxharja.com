1817b4f1d7f60056da72eeceefaf1b6e0a4b4ed3
eneax <19818937+eneax@users.noreply.github.com>
2019-10-15 10:58:06 +0200

    Refactor data folder

--- src/data/index.js
+++
@@ -1,6 +0,0 @@
-import books from './booksData'
-import funResources from './funResourcesData'
-import projects from './projectsData'
-import travels from './travelsData'
-
-export { books, funResources, projects, travels }
