58590390e3dd971a8a7595f8ffdabad7a4805592
eneax <19818937+eneax@users.noreply.github.com>
2019-10-15 10:56:00 +0200

    Refactor projects page to use useStaticQuery

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,22 +1,16 @@
-import React, { Fragment } from 'react'
-import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
 import Footer from '../components/footer'
-import { Title, Container, ImageWrapper, SubTitle, ExternalLink } from '../elements'
-import { grey } from '../utils'
-import { projects } from '../data'
+import ProjectsList from '../components/projectsList'
+
+import { Title, Container } from '../elements'
 
 
 const Projects = ({ data }) => {
-
-  const imageGallery = Object.values(data)
-  const images = []
-
-  imageGallery.map((img, i) => (
-    images.push(<Img key={i} fluid={img.childImageSharp.fluid} alt={`Image of '${projects[i].title}' project`} />)
-  ))
+  const response = useStaticQuery(getProjectsData)
+  const projects = response.allProjectsDataJson.edges
 
   return (
     <Layout>
@@ -24,29 +18,7 @@ const Projects = ({ data }) => {
 
       <Container>
         <p>Explore some of my favorite projects I've worked on over the last few months.</p>
-        <Fragment>
-          {
-            projects.map((project, i) => (
-
-              <div key={i}>
-                <ExternalLink href={`${projects[i].link}`}>
-                  <SubTitle>{projects[i].title}</SubTitle>
-                </ExternalLink>
-
-                <ImageWrapper>
-                  <ExternalLink href={`${projects[i].link}`}>
-                    {
-                      images[i]
-                    }
-                  </ExternalLink>
-                </ImageWrapper>
-
-                <p style={{color: `${grey}`}}>{projects[i].desc}</p>
-              </div>
-
-            ))
-          }
-        </Fragment>
+        <ProjectsList projects={projects} />
       </Container>
 
       <Footer />
@@ -57,101 +29,24 @@ const Projects = ({ data }) => {
 export default Projects
 
 
-export const fluidImage = graphql`
-  fragment fluidImage on File {
-    childImageSharp {
-      fluid(maxWidth: 1240) {
-        ...GatsbyImageSharpFluid
+// query
+const getProjectsData = graphql`
+  {
+    allProjectsDataJson {
+      edges {
+        node {
+          link
+          title
+          img {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid_tracedSVG
+              }
+            }
+          }
+          desc
+        }
       }
-    }
-  }
-`
-
-export const query = graphql`
-  query imageGallery {
-    image28: file(relativePath: { regex: "/28.png/" }) {
-      ...fluidImage
-    }
-    image27: file(relativePath: { regex: "/27.png/" }) {
-      ...fluidImage
-    }
-    image26: file(relativePath: { regex: "/26.png/" }) {
-      ...fluidImage
-    }
-    image25: file(relativePath: { regex: "/25.png/" }) {
-      ...fluidImage
-    }
-    image24: file(relativePath: { regex: "/24.png/" }) {
-      ...fluidImage
-    }
-    image23: file(relativePath: { regex: "/23.png/" }) {
-      ...fluidImage
-    }
-    image22: file(relativePath: { regex: "/22.png/" }) {
-      ...fluidImage
-    }
-    image21: file(relativePath: { regex: "/21.png/" }) {
-      ...fluidImage
-    }
-    image20: file(relativePath: { regex: "/20.png/" }) {
-      ...fluidImage
-    }
-    image19: file(relativePath: { regex: "/19.png/" }) {
-      ...fluidImage
-    }
-    image18: file(relativePath: { regex: "/18.png/" }) {
-      ...fluidImage
-    }
-    image17: file(relativePath: { regex: "/17.png/" }) {
-      ...fluidImage
-    }
-    image16: file(relativePath: { regex: "/16.png/" }) {
-      ...fluidImage
-    }
-    image15: file(relativePath: { regex: "/15.png/" }) {
-      ...fluidImage
-    }
-    image14: file(relativePath: { regex: "/14.png/" }) {
-      ...fluidImage
-    }
-    image13: file(relativePath: { regex: "/13.png/" }) {
-      ...fluidImage
-    }
-    image12: file(relativePath: { regex: "/12.png/" }) {
-      ...fluidImage
-    }
-    image11: file(relativePath: { regex: "/11.png/" }) {
-      ...fluidImage
-    }
-    image10: file(relativePath: { regex: "/10.png/" }) {
-      ...fluidImage
-    }
-    image9: file(relativePath: { regex: "/09.png/" }) {
-      ...fluidImage
-    }
-    image8: file(relativePath: { regex: "/08.png/" }) {
-      ...fluidImage
-    }
-    image7: file(relativePath: { regex: "/07.png/" }) {
-      ...fluidImage
-    }
-    image6: file(relativePath: { regex: "/06.png/" }) {
-      ...fluidImage
-    }
-    image5: file(relativePath: { regex: "/05.png/" }) {
-      ...fluidImage
-    }
-    image4: file(relativePath: { regex: "/04.png/" }) {
-      ...fluidImage
-    }
-    image3: file(relativePath: { regex: "/03.png/" }) {
-      ...fluidImage
-    }
-    image2: file(relativePath: { regex: "/02.png/" }) {
-      ...fluidImage
-    }
-    image1: file(relativePath: { regex: "/01.png/" }) {
-      ...fluidImage
     }
   }
 `
