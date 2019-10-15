73ee17af06fe1c6d20167f4d6f7c73affb027514
eneax <19818937+eneax@users.noreply.github.com>
2019-10-15 18:44:10 +0200

    Refactor about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,8 @@
 import React from 'react'
+
 import Layout from '../components/layout'
 import Footer from '../components/footer'
+
 import { Title, Container, ExternalLink, InternalLink } from '../elements'
 
 
