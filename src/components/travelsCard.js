f01c4a368b7a5c46aff6a7cfa01df446d4db44c9
eneax <19818937+eneax@users.noreply.github.com>
2019-10-15 10:53:14 +0200

    Create travelsCard component

---
+++ src/components/travelsCard.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react'
+import Img from 'gatsby-image'
+
+import { ImageWrapper } from '../elements'
+import { DateTime } from '../utils'
+
+
+const TravelsCard = ({ item }) => {
+  const { title } = item
+  const img = item.img.childImageSharp.fluid
+
+  return (
+    <Fragment>
+      <ImageWrapper>
+        <Img 
+          fluid={img}
+          alt={`Image of ${title}`}
+        />
+      </ImageWrapper>
+      <DateTime style={{marginBottom: '3rem'}}>{title}</DateTime>
+    </Fragment>
+  )
+}
+
+export default TravelsCard
