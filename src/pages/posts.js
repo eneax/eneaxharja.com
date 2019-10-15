0721aecfbdc35cb12c22ad0806694ecadc6f3712
eneax <19818937+eneax@users.noreply.github.com>
2019-10-15 18:44:49 +0200

    Refactor Posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,7 +1,9 @@
 import React from 'react'
+
 import Layout from '../components/layout'
 import PostsList from '../components/postsList'
 import Footer from '../components/footer'
+
 
 const Posts = () => (
   <Layout>
