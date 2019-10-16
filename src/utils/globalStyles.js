9a03ab390f144458b4fd0730fe350337996d52ff
eneax <19818937+eneax@users.noreply.github.com>
2019-10-16 10:33:44 +0200

    Add fade-in styles

--- src/utils/globalStyles.js
+++ src/utils/globalStyles.js
@@ -2,6 +2,24 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
   ::selection { background: #ab98d0; }
+
+  /* Fade-In Animation */
+  .fade-in-section {
+    opacity: 0;
+    transform: translateY(20vh);
+    visibility: hidden;
+    transition: opacity 1200ms ease-out, 
+                transform 600ms ease-out,
+                visibility 1200ms ease-out;
+    will-change: opacity, transform, visibility;
+  }
+  .fade-in-section.is-visible {
+    opacity: 1;
+    transform: none;
+    visibility: visible;
+  }
+
+  /* General */
   html {
     font-family: sans-serif;
     -ms-text-size-adjust: 100%;
