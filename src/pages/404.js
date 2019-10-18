fed76ccda6a0a880d209b179d440ed7876423c83
eneax <19818937+eneax@users.noreply.github.com>
2019-10-18 21:00:45 +0200

    Use SEO component in 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 import { Title, Container } from '../elements'
 
 
 const NotFoundPage = () => (
 	<Layout>
+		<SEO title='404: Not found' />
+
 		<Title>NOT FOUND</Title>
 
 		<Container>
