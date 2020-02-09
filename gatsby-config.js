0680e4723fa3feafe6646bc669663e8bfef7fe49
eneax <19818937+eneax@users.noreply.github.com>
2020-02-09 22:01:22 +0100

    Remove match from feed rss

--- gatsby-config.js
+++ gatsby-config.js
@@ -135,7 +135,7 @@ module.exports = {
             // if `string` is used, it will be used to create RegExp and then test if pathname of
             // current page satisfied this regular expression;
             // if not provided or `undefined`, all pages will have feed reference inserted
-            match: "^/posts/"
+            // match: "^/posts/"
           }
         ]
       }
