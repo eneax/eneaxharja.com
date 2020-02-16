0607d6bb34f213fa96621fb6451232ecdc5f3556
eneax <19818937+eneax@users.noreply.github.com>
2020-02-16 20:58:55 +0100

    Create tags automatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,11 @@
 const path = require('path')
+const _ = require("lodash")
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
   const blogPostTemplate = path.resolve('./src/components/postLayout.js')
+  const tagTemplate = path.resolve("src/components/tags.js")
 
   const result = await graphql(`
     { 
@@ -16,14 +18,21 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           }
         }
       }
+      tagsGroup: allMdx(limit: 2000) {
+        group(field: frontmatter___tags) {
+          fieldValue
+        }
+      }
     }
   `)
+
   // handle errors
   if (result.errors) {
     reporter.panicOnBuild(`Error while running GraphQL query.`)
     return
   }
 
+  // Extract posts data from query
   const posts = result.data.posts.edges
   // Create post detail pages
   posts.forEach(({ node }) => {
@@ -33,6 +42,19 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       context: {
         slug: node.frontmatter.slug
       }
+    })
+  })
+
+  // Extract tag data from query
+  const tags = result.data.tagsGroup.group
+  // Make tag pages
+  tags.forEach(tag => {
+    createPage({
+      path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
+      component: tagTemplate,
+      context: {
+        tag: tag.fieldValue,
+      },
     })
   })
 }
