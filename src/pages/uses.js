859b2fa5a1edf38a733bbd1a55a0a6dc2644f5a9
eneax <19818937+eneax@users.noreply.github.com>
2020-04-04 13:08:15 +0200

    Update uses page

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -24,12 +24,16 @@ const Uses = () => (
     </Title>
 
     <Container>
-      <p>Ever wonder about what gear I use? Here is my complete list.</p>
+      <p>
+        Ever wonder about what gear I use? Here is a complete list of my
+        favorite hardware and software.
+      </p>
 
       <SubTitle>Hardware</SubTitle>
       <UnOrderedList>
         <li>MacBook Pro (15-inch, 2016)</li>
         <li>Google Pixel 3a</li>
+        <li>Google Stadia</li>
         <li>Kindle (10th Generation)</li>
       </UnOrderedList>
 
@@ -66,6 +70,11 @@ const Uses = () => (
       <SubTitle>Chrome Extensions</SubTitle>
       <UnOrderedList>
         <li>
+          <ExternalLink href="https://chrome.google.com/webstore/detail/visbug/cdockenadnadldjbbgcallicgledbeoc">
+            VisBug
+          </ExternalLink>
+        </li>
+        <li>
           <ExternalLink href="https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi?utm_source=chrome-ntp-icon">
             React Developer Tools
           </ExternalLink>
@@ -85,9 +94,33 @@ const Uses = () => (
             uBlock Origin
           </ExternalLink>
         </li>
+      </UnOrderedList>
+
+      <SubTitle>Progressive Web Apps</SubTitle>
+      <UnOrderedList>
         <li>
-          <ExternalLink href="https://chrome.google.com/webstore/detail/1password-x-%E2%80%93-password-ma/aeblfdkhhhdcdjpifhhbdiojplfjncoa?utm_source=chrome-ntp-icon">
-            1Password X
+          <ExternalLink href="https://duo.google.com">Duo</ExternalLink>
+        </li>
+        <li>
+          <ExternalLink href="https://messages.google.com">
+            Messages
+          </ExternalLink>
+        </li>
+        <li>
+          <ExternalLink href="https://maps.google.com">Maps</ExternalLink>
+        </li>
+        <li>
+          <ExternalLink href="https://photos.google.com">Photos</ExternalLink>
+        </li>
+        <li>
+          <ExternalLink href="https://drive.google.com">Drive</ExternalLink>
+        </li>
+        <li>
+          <ExternalLink href="https://stadia.google.com">Stadia</ExternalLink>
+        </li>
+        <li>
+          <ExternalLink href="https://music.youtube.com">
+            YouTube Music
           </ExternalLink>
         </li>
       </UnOrderedList>
