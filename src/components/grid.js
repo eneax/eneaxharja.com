124b61f91b71f3bc23342451d9c63ab2d79129c1
eneax <19818937+eneax@users.noreply.github.com>
2020-12-22 11:49:59 +0100

    Redesign homepage

---
+++ src/components/grid.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+
+import { ExternalLink } from '../utils/hyperLinks';
+import { darkTheme } from '../utils/colors';
+
+// styles
+const GridContainer = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  gap: 1rem;
+
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const CardContainer = styled.div`
+  padding: 1rem;
+  border: 1px solid ${darkTheme.textLighter};
+  border-radius: 0.25rem;
+
+  h3 {
+    margin-top: 0;
+    color: ${darkTheme.textLighter};
+  }
+
+  ul {
+    list-style-type: none;
+    margin-left: 0;
+  }
+`;
+
+const Grid = () => (
+  <GridContainer>
+    <CardContainer>
+      <h3>Notes</h3>
+      <ul>
+        <li>
+          <Link to="/javascript">JavaScript</Link>
+        </li>
+        <li>
+          <Link to="/react">React</Link>
+        </li>
+        <li>
+          <Link to="/gatsby">Gatsby</Link>
+        </li>
+        <li>
+          <Link to="/jamstack">Jamstack</Link>
+        </li>
+      </ul>
+    </CardContainer>
+
+    <CardContainer>
+      <h3>Projects</h3>
+      <ul>
+        <li>
+          <Link to="/coffee-shop">coffee-shop</Link>
+        </li>
+        {/* <li>gatsby-dev-portfolio</li>
+        <li>gatsby-restaurant</li>
+        <li>globetrotter</li> */}
+        <li>
+          <Link to="/projects">More projects</Link>
+        </li>
+      </ul>
+    </CardContainer>
+
+    <CardContainer>
+      <h3>Fun</h3>
+      <ul>
+        <li>
+          <Link to="/books">Books</Link>
+        </li>
+        <li>
+          <ExternalLink href="https://www.youtube.com/c/EneaXharja17">
+            Video Games
+          </ExternalLink>
+        </li>
+        <li>
+          <Link to="/travel">Travel</Link>
+        </li>
+      </ul>
+    </CardContainer>
+
+    <CardContainer>
+      <h3>Miscellany</h3>
+      <ul>
+        <li>
+          <Link to="/resume">Resume</Link>
+        </li>
+        <li>
+          <Link to="/uses">Uses</Link>
+        </li>
+        <li>
+          <Link to="/about-this-site">About this site</Link>
+        </li>
+      </ul>
+    </CardContainer>
+  </GridContainer>
+);
+
+export default Grid;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,33 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Hero from '../components/hero';
-import RecentPosts from '../components/recentPosts';
-import RecentProjects from '../components/recentProjects';
+import Grid from '../components/grid';
+
+import { ExternalLink } from '../utils/hyperLinks';
 
 const HomePage = () => (
   <Layout>
     <SEO title="Home" />
-
     <Hero />
-    <RecentPosts />
-    <RecentProjects />
+    <p>
+      I work at <ExternalLink href="https://eqolot.com">eqolot</ExternalLink>{' '}
+      during the day and spend most of my time writing{' '}
+      <ExternalLink href="https://github.com/eneax">code</ExternalLink> for the
+      web. I create fast, highly performing and accessible{' '}
+      <ExternalLink href="https://jamstack.org">Jamstack</ExternalLink> websites
+      that are responsive and can adapt the layout to a variety of devices and
+      screen sizes.
+    </p>
+
+    <Grid />
+
+    <p>
+      I'm open to new opportunities and always looking for new projects where I
+      can contribute (anything built on Jamstack). You can find me on the good
+      old-fashioned{' '}
+      <ExternalLink href="mailto:eneaxharja@gmail.com">email</ExternalLink>. It
+      just might take a few days for me to get back to you.
+    </p>
   </Layout>
 );
 
