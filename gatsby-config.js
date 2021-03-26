b830f6db5f91159d6e9db0270b00eca6718de2be
eneax <19818937+eneax@users.noreply.github.com>
2021-03-26 19:36:31 +0100

    Update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,11 +108,32 @@ module.exports = {
       options: {
         name: `Enea Xharja`,
         short_name: `Enea`,
+        description: `My Own Little Corner of the Web!`,
         start_url: `/`,
-        background_color: `rgba(255, 255, 255, 1)`,
-        theme_color: `rgba(252, 163, 17, 1)`,
+        background_color: `#ffffff`,
+        theme_color: `#FCA311`,
         display: 'standalone',
         icons: [
+          {
+            src: '/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+          {
+            src: '/mstile-150x150.png',
+            sizes: '150x150',
+            type: 'image/png',
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
           {
             src: '/android-chrome-192x192.png',
             sizes: '192x192',
