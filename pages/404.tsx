b802305142d930dbc97194ec82b7b05a63b6a9f3
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-03-26 18:38:14 +0200

    refactor: improve NotFound page structure

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,12 +4,10 @@ import Container from "components/Container";
 
 const NotFound: NextPage = () => (
   <Container title="404 - Enea Xharja">
-    <div className="flex flex-col justify-center border-gray-700 mx-auto w-full pt-16 ">
-      <div className="flex flex-col min-h-[calc(100vh-17rem)]">
-        <h1 className="mb-8">404: Not Found</h1>
-        <p>It seems you are looking for a page that does not exist.</p>
-      </div>
-    </div>
+    <article className="flex flex-col mt-16 min-h-[calc(100vh-17rem)]">
+      <h1 className="mb-4">404: Not Found</h1>
+      <p>It seems you are looking for a page that does not exist.</p>
+    </article>
   </Container>
 );
 
