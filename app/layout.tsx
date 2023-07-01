3bb7c271dab2199d22ff7fb8ddbab4fe779a2a7c
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-07-01 11:36:56 +0200

    feat: update nav with link to archives

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,30 +34,25 @@ export default function RootLayout({
         className={`max-w-2xl mx-auto flex flex-col justify-center px-8 md:px-0 bg-gray-900 text-primary-50 ${inter.className}`}
       >
         <header>
-          <nav className="py-8">
-            <div className="flex items-center space-x-6">
-              <Link
-                href="/"
-                title="Enea Xharja"
-                className="rounded-full flex items-center space-x-2 no-underline"
-              >
-                <div className="rounded-full shadow-lg p-[1px] bg-primary-50 group transform transition ease-out hover:scale-105">
-                  <div className="rounded-full p-[1px] h-[36px] w-[36px] transition duration-300 group-hover:scale-105">
-                    <Image
-                      src="/images/avatar.png"
-                      alt="A photo of Enea Xharja"
-                      quality={95}
-                      className="rounded-full"
-                      width={36}
-                      height={36}
-                    />
-                  </div>
+          <nav className="flex items-center justify-between py-8">
+            <Link href="/" title="Enea Xharja">
+              <div className="rounded-full shadow-lg p-[1px] bg-primary-50 group transform transition ease-out hover:scale-105">
+                <div className="rounded-full p-[1px] h-[40px] w-[40px] transition duration-300 group-hover:scale-105">
+                  <Image
+                    src="/images/avatar.png"
+                    alt="A photo of Enea Xharja"
+                    quality={95}
+                    className="rounded-full"
+                    width={40}
+                    height={40}
+                  />
                 </div>
-                <span className="tracking-tight font-semibold text-xl text-primary-50">
-                  Enea Xharja
-                </span>
-              </Link>
-            </div>
+              </div>
+            </Link>
+
+            <Link href="/archives" className="btn">
+              Archives
+            </Link>
           </nav>
         </header>
 
