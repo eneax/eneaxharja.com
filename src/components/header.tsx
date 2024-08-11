d870b25040005c928bf4cf1497ae303fc43056f6
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-08-11 13:40:46 +0200

    refactor: update layout to fix footer overlap content issue

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="antialiased max-w-xl my-16 mx-4 md:mx-auto tracking-tight flex flex-col items-start">
+    <header className="antialiased max-w-xl md:w-full mt-16 mb-8 mx-4 md:mx-auto tracking-tight flex flex-col items-start">
       <Link href="/">
         <h1 className="font-bold text-neutral-100">Enea Xharja</h1>
         <span className="text-neutral-400 text-sm">Software Engineer</span>
